f5670dcd2ea888f67b7d0c09def7dbc63f855088
Takayuki Shimada <taka@tsmd.jp>
2019-09-29 23:51:44 +0900

    コメントを追記する

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,32 @@ var rename = require('gulp-rename')
 var source = require('vinyl-source-stream')
 var tap = require('gulp-tap')
 
-// 共通の JS モジュール
-var commonModules = [
-  'js-cookie',
-  'underscore',
-  { file: './src/assets/javascripts/common/string-utils', expose: 'string-utils' }
-]
-
 var js = function() {
-  var commonBundler
-  commonBundler = browserify('src/assets/javascripts/common.js')
-  commonBundler.require(commonModules)
+  var commonBundler = browserify('src/assets/javascripts/common.js')
+
+  // 共通の JS モジュール
+  // すべての画面で共通して読み込ませる JS ファイルをここに記述する。
+  // jQuery, Underscore.js, 各種ユーティリティ関数や、
+  // ヘッダー、フッター等の全画面共通の UI が必要とするスクリプト等が想定される
+  commonBundler.require([
+    // NPM パッケージは、パッケージ名を書く。
+    // npm install --save-dev でインストールすることを忘れない。
+    'js-cookie',
+    'underscore',
+    // ローカルのファイルは、gulpfile.js からの相対パスを記述する。
+    // expose プロパティで指定した名前で、require できるようになる。
+    { file: './src/assets/javascripts/common/string-utils', expose: 'string-utils' }
+  ])
+
+  // 全画面共通で読み込む JS ファイル (common.bundle.js) を生成する
   var commonStream = commonBundler
     .bundle()
     .pipe(source('common.bundle.js'))
     .pipe(gulp.dest('htdocs/assets/javascripts'))
 
+  // ページ固有の処理を行う JS ファイル ([page].bundle.js) を生成する
+  // 以下の書き方は Gulp 公式のレシピから拝借
+  // https://github.com/gulpjs/gulp/blob/master/docs/recipes/browserify-multiple-destination.md
   var pageStream = gulp
     .src('src/assets/javascripts/pages/*.js', { read: false })
     .pipe(
@@ -36,6 +46,8 @@ var js = function() {
       })
     )
     .pipe(gulp.dest('htdocs/assets/javascripts/pages'))
+
+  // ２つの browserify タスクをまとめる
   return merge(commonStream, pageStream)
 }
 
